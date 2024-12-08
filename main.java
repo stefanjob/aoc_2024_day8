1a5a674841763579413a83830857377a2ef72ef4
Stefan Job <stefan.job@me.com>
2024-12-08 17:43:35 +0100

    part2 done

--- main.java
+++ main.java
@@ -68,6 +68,70 @@ public class main {
         return antinodes.size();
     }
 
+    public static int countUniqueAntinodes2(String[] map) {
+        int rows = map.length;
+        int cols = map[0].length();
+
+        // Store antenna locations grouped by frequency
+        Map<Character, List<int[]>> antennaLocations = new HashMap<>();
+
+        // Populate antenna locations
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                char ch = map[r].charAt(c);
+                if (ch != '.') {
+                    antennaLocations.putIfAbsent(ch, new ArrayList<>());
+                    antennaLocations.get(ch).add(new int[]{r, c});
+                }
+            }
+        }
+
+        // Set to store unique antinode locations
+        Set<String> antinodes = new HashSet<>();
+
+        // Calculate antinodes for each frequency group
+        for (Map.Entry<Character, List<int[]>> entry : antennaLocations.entrySet()) {
+            List<int[]> antennas = entry.getValue();
+
+            // Add all antenna locations as antinodes
+            for (int[] antenna : antennas) {
+                antinodes.add(antenna[0] + "," + antenna[1]);
+            }
+
+            // Check all pairs of antennas of the same frequency
+            for (int i = 0; i < antennas.size(); i++) {
+                for (int j = i + 1; j < antennas.size(); j++) {
+                    int[] a1 = antennas.get(i);
+                    int[] a2 = antennas.get(j);
+
+                    // Mark all grid positions in line with both antennas
+                    int dr = a2[0] - a1[0];
+                    int dc = a2[1] - a1[1];
+
+                    int nextR = a1[0];
+                    int nextC = a1[1];
+
+                    while (isInBounds(nextR, nextC, rows, cols)) {
+                        antinodes.add(nextR + "," + nextC);
+                        nextR += dr;
+                        nextC += dc;
+                    }
+
+                    nextR = a1[0] - dr;
+                    nextC = a1[1] - dc;
+
+                    while (isInBounds(nextR, nextC, rows, cols)) {
+                        antinodes.add(nextR + "," + nextC);
+                        nextR -= dr;
+                        nextC -= dc;
+                    }
+                }
+            }
+        }
+
+        return antinodes.size();
+    }
+
     private static boolean isInBounds(int r, int c, int rows, int cols) {
         return r >= 0 && r < rows && c >= 0 && c < cols;
     }
@@ -75,7 +139,7 @@ public class main {
     public static void part1() {
         System.out.println("AoC Day 8 Part 1");
        
-        boolean full = true; 
+        boolean full = false; 
         List<String> lines = new ArrayList<>();
 
         try {
@@ -94,8 +158,31 @@ public class main {
           
         System.out.println("Unique antinode locations: " + countUniqueAntinodes(map));
     }
+
+    public static void part2() {
+        System.out.println("AoC Day 8 Part 2");
+       
+        boolean full = true; 
+        List<String> lines = new ArrayList<>();
+
+        try {
+            if (full) {
+                lines = Files.readAllLines(Paths.get("input_full.txt"));
+            } else {
+                lines = Files.readAllLines(Paths.get("input_test.txt"));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+        map = new String[lines.size()];
+        map = lines.toArray(String[]::new);
+          
+        System.out.println("Unique antinode locations: " + countUniqueAntinodes2(map));
+    }
  
    public static void main(String[] args) {
-    part1();
+    part2();
    }
 }
